65460047df4d98a24810904dbfcb9384ec246be3
Jason <jasonjordaan10@gmail.com>
2023-07-27 10:24:43 +0200

    Alpha stage

--- Kronos/time.js
+++ Kronos/time.js
@@ -5,14 +5,14 @@ var tomorrow = require("date-and-time")
 async function job() {
   try {
     var birb = await connect(); //Connect to mongo
-    var time = new Date(); //Gets the current date and time
-    var form = tomorrow.format(time, "YYY-MM-DD")
+    var time = new Date(); //Instantiate a Date object
+    var form = tomorrow.format(time, "YYY-MM-DD") //Format the form
     var pend = await birb.collection("tasks").find({
       status: "pending",
       next_execute_date_time: { $lte: form },
     }).toArray() //Mongodb find query based on status and next_execute_date_time params. Returns a cursor converted to Array
 
-    for (var task of pend) {
+    for (var task of pend) { //Iterates through the Array and makes changes accordingly
       await birb.collection("tasks").updateOne(
         { _id: task._id },
         { $set: { status: "done" } }
